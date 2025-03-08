7927dd410e820c2a85455cc4f2aa28f59ae44d66
Basel Hussain <baselhussain4@gmail.com>
2025-03-09 04:44:48 +0500

    Update pass.py

--- pass-strength-checker/pass.py
+++ pass-strength-checker/pass.py
@@ -12,36 +12,41 @@ st.markdown("""
 password =st.text_input("Enter your password", type="password")
 feedback =[]
 score=0
+reject_passwords=["password123","hello342","123456789"]
 
 if password:
-    if len(password) >=8:
-        score+=1
+    if password.lower() in reject_passwords:
+        feedback.append("❌This password can't be used - it's in the list of commonly used passwords")
+        score = 0
     else:
-        feedback.append("❌Password should be atleast 8 characters long.")
+        if len(password) >=8:
+            score+=1
+        else:
+            feedback.append("❌Password should be atleast 8 characters long.")
 
-    if re.search(r'[A-Z]',password ) and re.search(r'[a-z]', password):
-        score +=1
-    else:
-        feedback.append("❌Password should contain both upper and lower case characters.")
+        if re.search(r'[A-Z]',password ) and re.search(r'[a-z]', password):
+            score +=1
+        else:
+            feedback.append("❌Password should contain both upper and lower case characters.")
 
-    if re.search(r'[0-9]', password):
-        score+=1
-    else:
-        feedback.append("❌Password should contain atleast one digit.")
+        if re.search(r'[0-9]', password):
+            score+=1
+        else:
+            feedback.append("❌Password should contain atleast one digit.")
 
-    if re.search(r'[!@#$%&]', password):
-        score+=1
-    else:
-        feedback.append("❌Password should contain atleast one special characters(!@#$%&).")
+        if re.search(r'[!@#$%&]', password):
+            score+=1
+        else:
+            feedback.append("❌Password should contain atleast one special characters(!@#$%&).")
 
     if score==4:
         feedback.append("✅Your password is strong enough")
     elif score==3:
         feedback.append("Your password is medium strenght. It could be more strong")
-    elif score<3:
+    else:
         special_chars = "!@#$%&"
         characters = string.ascii_letters + string.digits + special_chars
-        passwordGive="".join(random.choice(characters) for _ in range(8))
+        passwordGive = "".join(random.choice(characters) for _ in range(8))
         feedback.append(f"Your password is weak. Please make it strong. Create password like {passwordGive}")
 
     if feedback:
